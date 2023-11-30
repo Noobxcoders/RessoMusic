609f9448f65645643f409d2d0113c6a571964299
Noobxcoders <128674386+Noobxcoders@users.noreply.github.com>
2023-11-30 15:51:18 +0530

    Update active.py

--- YukkiMusic/plugins/tools/active.py
+++ YukkiMusic/plugins/tools/active.py
@@ -2,9 +2,9 @@ from pyrogram import filters
 from pyrogram.types import Message
 from unidecode import unidecode
 
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.database import (
+from YukkiMusic import app
+from YukkiMusic.misc import SUDOERS
+from YukkiMusic.utils.database import (
     get_active_chats,
     get_active_video_chats,
     remove_active_chat,
